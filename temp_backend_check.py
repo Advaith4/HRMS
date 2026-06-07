43e8889dd839407547c43e51b64ffe9d7eca257b
AlphaVlogger <arunkumaar16.yt@gmail.com>
2026-06-07 15:06:07 +0530

    Cleanup Phase-2

--- temp_backend_check.py
+++
@@ -1,7 +0,0 @@
-import requests
-try:
-    r = requests.get('http://127.0.0.1:8000', timeout=5)
-    print('BACKEND', r.status_code)
-    print(r.text[:300])
-except Exception as e:
-    print('BACKEND ERROR', e)
